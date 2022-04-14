fbbc725bb0a27f53d34dca11c2fbe5d3d50f7d20
sdkiq1234 <sdkiq123@naver.com>
2022-04-14 12:23:53 +0900

    first

---
+++ screens/Tv.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native-web";
+
+const Tv = () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>Tv</Text>
+  </View>
+);
+
+export default Tv;
