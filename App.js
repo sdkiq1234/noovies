267af137b9b1ab8e4e3fa8dac482d1c580e287f5
sdkiq1234 <sdkiq123@naver.com>
2022-04-22 17:41:47 +0900

    section1

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
-import AppLoading from "expo-app-loading";
-import React, { useState } from "react";
-import * as Font from "expo-font";
-import { Image } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
-import { NavigationContainer } from "@react-navigation/native";
-import Tabs from "./navigation/Tabs";
+import AppLoading from 'expo-app-loading';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import { Image, useColorScheme } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Asset } from 'expo-asset';
+import { NavigationContainer } from '@react-navigation/native';
+import Root from './navigation/Root';
+import { ThemeProvider } from 'styled-components/native';
+import { darkTheme, lightTheme } from './styled';
 
 const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-
 const loadImages = (images) =>
   images.map((image) => {
-    if (typeof image === "string") {
+    if (typeof image === 'string') {
       return Image.prefetch(image);
     } else {
       return Asset.loadAsync(image);
     }
   });
-
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
@@ -25,6 +25,7 @@ export default function App() {
     const fonts = loadFonts([Ionicons.font]);
     await Promise.all([...fonts]);
   };
+  const isDark = useColorScheme() === 'light';
   if (!ready) {
     return (
       <AppLoading
@@ -35,8 +36,10 @@ export default function App() {
     );
   }
   return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,15 +3,65 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Movies from '../screens/Movies';
 import Tv from '../screens/Tv';
 import Search from '../screens/Search';
+import { useColorScheme } from 'react-native';
+import { BLACK_COLOR, YELLOW_COLOR, DARK_GREY, LIGHT_GREY } from '../colors';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} />
-    <Tab.Screen name="Tv" component={Tv} />
-    <Tab.Screen name="Search" component={Search} />
-  </Tab.Navigator>
-);
+const Tabs = () => {
+  const isDark = useColorScheme() === 'light';
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : 'white',
+        },
+        tabBarActiveTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
+        tabBarInactiveTintColor: isDark ? DARK_GREY : LIGHT_GREY,
+        headerStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : 'white',
+        },
+        headerTitleStyle: {
+          color: isDark ? 'white' : BLACK_COLOR,
+        },
+        tabBarLabelStyle: {
+          fontSize: 10,
+          fontWeight: '600',
+          marginTop: -10,
+          marginBottom: 5,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={'film-outline'} color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Tv"
+        component={Tv}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={'tv-outline'} color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name={'search-outline'} color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
